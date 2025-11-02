2cb4b9cd12cb4a5a34e1dd531c927a0d2e10d43f
Priyangshu <contact.priyangshu.work@gmail.com>
2025-11-02 22:50:39 +0530

    Task schema

---
+++ src/models/task.models.js
@@ -0,0 +1,47 @@
+import mongoose, { Schema } from "mongoose";
+
+const taskSchema = new Schema(
+    {
+        project_id: {
+            type: Schema.Types.ObjectId,
+            ref: "Project"
+        },
+        task_name: {
+            type: String,
+            trim: true
+        },
+        description: {
+            type: String,
+            trim: true
+        },
+        priority: {
+            type: String,
+            trim: true
+        },
+        status: {
+            type: String,
+            trim: true
+        },
+        assigned_to: {
+            type: Schema.type.ObjectId,
+            ref: "User"
+        },
+        created_by: {
+            type: Schema.type.ObjectId,
+            ref: "User"
+        },
+        parent_task_id: {
+            type: Schema.type.ObjectId,
+            ref: "Project"
+        },
+        due_date: {
+            type: Date
+        },
+        estimated_time: {
+            type: Number
+        },
+    },
+    {
+        timestamps: true
+    }
+)
