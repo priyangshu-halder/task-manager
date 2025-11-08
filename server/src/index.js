2c562ed225e95f6f7594a6900f8bde504caa92cc
Priyangshu <contact.priyangshu.work@gmail.com>
2025-11-08 18:24:54 +0530

    database Connection

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,19 @@
+import dotenv from 'dotenv'
 import app from './app.js'
+import connectDB from './db/dbConnection.js'
 
-app.listen(8001, () => {
-  console.log('ADSas')
+dotenv.config({
+  path: './.env',
 })
+const port = process.env.PORT || 8001
+
+connectDB()
+  .then(
+    app.listen(port, () => {
+      console.log(`App is running at ${port}`)
+    })
+  )
+  .catch(error => {
+    console.error('Mongodb connection error', error)
+    process.exit(1)
+  })
